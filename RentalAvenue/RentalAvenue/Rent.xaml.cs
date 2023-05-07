8eb1b0b860eab42eb154b6bb9415731aa392e1f3
Ksenia <kseni.zhyk.3@gmail.com>
2023-05-07 18:02:28 +0300

    Kursach
    Commitimg main list for project

---
+++ RentalAvenue/RentalAvenue/Rent.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RentalAvenue
+{
+    /// <summary>
+    /// Логика взаимодействия для Rent.xaml
+    /// </summary>
+    public partial class Rent : Page
+    {
+        public Rent()
+        {
+            InitializeComponent();
+            this.Width = 500;
+            this.Height = 500;
+            this.HorizontalAlignment = HorizontalAlignment.Stretch;
+            this.VerticalAlignment = VerticalAlignment.Stretch;
+
+        }
+        private void OnSubmit(object sender, RoutedEventArgs e)
+        {
+            // Создаем новый объект жилья и заполняем его данными из формы
+            RentalProperty property = new RentalProperty();
+            property.PropertyType = PropertyTypeComboBox.Text;
+            property.Address = AddressTextBox.Text;
+            property.Price = Convert.ToDouble(PriceTextBox.Text);
+            property.Rooms = Convert.ToInt32(RoomsComboBox.Text);
+            property.Description = DescriptionTextBox.Text;
+
+            // Добавляем объект в базу данных
+            //using (var db = new RentalDbContext())
+            //{
+            //    db.RentalProperties.Add(property);
+            //    db.SaveChanges();
+            //}
+
+            // Очищаем форму для следующего ввода
+            PropertyTypeComboBox.SelectedIndex = 0;
+            AddressTextBox.Clear();
+            PriceTextBox.Clear();
+            RoomsComboBox.SelectedIndex = 0;
+            DescriptionTextBox.Clear();
+
+            // Показываем сообщение об успешной отправке данных
+            MessageBox.Show("Your rental property has been submitted successfully.");
+        }
+    }
+}
---
+++ RentalAvenue/RentalAvenue/RentalProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalAvenue
+{
+    public class RentalProperty
+    {
+        public string PropertyType { get; set; }
+        public string Address { get; set; }
+        public double Price { get; set; }
+        public int Rooms { get; set; }
+        public string Description { get; set; }
+    }
+}
